02ba53f4d5b167c6a9b1eacddc2c620565deddc9
rawah <rawah@pdx.edu>
2020-12-31 23:30:50 -0800

    Updates

---
+++ precache-manifest.71fcbc5c7ea048c3d6e8c9095e437cda.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3761fad407fd77f61c507df420f9593c",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "b5ed317626464c5322bd",
+    "url": "/covid-dashboard/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "1785fa3edd93bf5cb7a2",
+    "url": "/covid-dashboard/static/css/main.eefb441c.chunk.css"
+  },
+  {
+    "revision": "b5ed317626464c5322bd",
+    "url": "/covid-dashboard/static/js/2.8a3da294.chunk.js"
+  },
+  {
+    "revision": "059566d8578fd7765dbe7d25720e3a66",
+    "url": "/covid-dashboard/static/js/2.8a3da294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1785fa3edd93bf5cb7a2",
+    "url": "/covid-dashboard/static/js/main.e114c022.chunk.js"
+  },
+  {
+    "revision": "aae4bc047a0b88ce9660",
+    "url": "/covid-dashboard/static/js/runtime-main.697d0302.js"
+  },
+  {
+    "revision": "6e5b429de4188ffe056c5ab396831821",
+    "url": "/covid-dashboard/static/media/mask.6e5b429d.svg"
+  },
+  {
+    "revision": "4e113c7e2f71c27ff4f71c6d1444df6c",
+    "url": "/covid-dashboard/static/media/virus.4e113c7e.svg"
+  }
+]);
